cccf1347daec5346650d28343d983d6977d61cac
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-09-11 17:23:08 -0300

    Terminar primer, segundo y tercer Div d info

--- src/apisModule.js
+++ src/apisModule.js
@@ -1,47 +1,74 @@
-import { encontradoDOM, errorLugarAcoordenadasDOM, errorInfoMeteoDOM, mostrarInfoMeteoDOM } from "./domModule";
+import {
+  encontradoDOM,
+  errorLugarAcoordenadasDOM,
+  errorInfoMeteoDOM,
+  mostrarInfoMeteoDOM,
+  mostrarInfoMeteoPronosticoDOM,
+  errorInfoMeteoPronosticoDOM
+} from "./domModule";
 
 const apiKey = "a89d97a5f8156e276d76abbdc33383e3";
-let unidades = "metric"; 
+let unidades = "metric";
 // ó imperial
 
 function lugarAcoordenadas(lugar) {
   let promesaData = fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${lugar}&limit=5&appid=${apiKey}`,
     { mode: "cors" }
-	);
-	
-	promesaData
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (response) {
-			encontradoDOM(response);
-			return(response);
-		})
-		.catch(function(error) {
-			errorLugarAcoordenadasDOM(error);
-			return("error");
-		})
+  );
+
+  promesaData
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      encontradoDOM(response);
+      return response;
+    })
+    .catch(function(error) {
+      errorLugarAcoordenadasDOM(error);
+      return "error";
+    });
 }
 
 function infoMeteo(lat, lon) {
-	let promesaData = fetch(
+  let promesaData = fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unidades}&lang=sp&appid=${apiKey}`,
     { mode: "cors" }
-	);
-	
-	promesaData
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (response) {
-			mostrarInfoMeteoDOM(response);
-			return(response);
-		})
-		.catch(function(error) {
-			errorInfoMeteoDOM(error);
-			return(error);
-		})
+  );
+
+  promesaData
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      mostrarInfoMeteoDOM(response);
+      return response;
+    })
+    .catch(function(error) {
+      errorInfoMeteoDOM(error);
+      return error;
+    });
 }
 
-export { lugarAcoordenadas, infoMeteo };
+function infoMeteoPronostico(lat, lon) {
+  let promesaData = fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${unidades}&lang=sp&appid=${apiKey}`,
+    { mode: "cors" }
+  );
+
+  promesaData
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      mostrarInfoMeteoPronosticoDOM(response);
+      return response;
+    })
+    .catch(function(error) {
+      errorInfoMeteoPronosticoDOM(error);
+      return error;
+    });
+}
+
+export { lugarAcoordenadas, infoMeteo, infoMeteoPronostico, unidades };
--- src/domModule.js
+++ src/domModule.js
@@ -1,4 +1,9 @@
-import { lugarAcoordenadas, infoMeteo } from "./apisModule";
+import {
+  lugarAcoordenadas,
+  infoMeteo,
+  infoMeteoPronostico,
+  unidades
+} from "./apisModule";
 
 const buscarInput = document.querySelector("input");
 
@@ -31,6 +36,15 @@ function encontradoDOM(response) {
     } else {
       nombre = response[0].name;
     }
+
+    if (response[0].hasOwnProperty("state")) {
+      nombre = `${nombre}, ${response[0].state}`;
+    }
+
+    if (response[0].hasOwnProperty("country")) {
+      nombre = `${nombre}, ${response[0].country}`;
+    }
+
     requestInfoMeteoDOM(nombre, response[0].lat, response[0].lon);
   } else if (response.length > 1) {
     contenedorInfo.textContent = `Se encontraron ${response.length} resultados:`;
@@ -51,7 +65,9 @@ function encontradoDOM(response) {
       resultado.classList.add("resultado");
       resultado.dataset.lat = response[index].lat;
       resultado.dataset.lon = response[index].lon;
+
       resultado.addEventListener("click", llamarRequestInfoMeteoDOM);
+
       resultado.textContent = `${nombre}`;
       if (response[index].hasOwnProperty("state")) {
         resultado.textContent =
@@ -65,7 +81,6 @@ function encontradoDOM(response) {
     }
     divBuscar.appendChild(resultados);
   }
-  console.log(response);
 }
 
 function llamarRequestInfoMeteoDOM() {
@@ -82,35 +97,211 @@ function requestInfoMeteoDOM(lugar, lat, lon) {
 
   const h2lugar = document.createElement("h2");
   h2lugar.textContent = lugar;
-	contenedorInfo.appendChild(h2lugar);
-	
-	const divCargando = document.createElement("div");
-	divCargando.classList.add("divCargando");
-	divCargando.textContent = "Cargando...";
-	contenedorInfo.appendChild(divCargando);
+  contenedorInfo.appendChild(h2lugar);
+
+  const divCargando = document.createElement("div");
+  divCargando.classList.add("divCargando");
+  divCargando.textContent = "Cargando...";
+  contenedorInfo.appendChild(divCargando);
 
   infoMeteo(lat, lon);
 }
 
 function mostrarInfoMeteoDOM(response) {
-	document.querySelector(".divCargando").remove();
-	console.log(response);
+  document.querySelector(".divCargando").remove();
+
+  // primerDiv
+  const primerDiv = document.createElement("div");
+  primerDiv.classList.add("primerDiv");
+
+  if (response.hasOwnProperty("weather")) {
+    response.weather.forEach(element => {
+      if (element.hasOwnProperty("description")) {
+        const descripcion = element.description;
+        const enMayus =
+          descripcion.slice(0, 1).toUpperCase() + descripcion.slice(1);
+        const divDescripcion = document.createElement("div");
+        divDescripcion.textContent = enMayus;
+        primerDiv.appendChild(divDescripcion);
+      }
+
+      const icon = document.createElement("div");
+      if (element.hasOwnProperty("icon")) {
+        const img = document.createElement("img");
+        img.src = `http://openweathermap.org/img/wn/${element.icon}@4x.png`;
+        icon.appendChild(img);
+      }
+      primerDiv.appendChild(icon);
+    });
+  }
+
+  let grados = "";
+  if (response.hasOwnProperty("main")) {
+    if (response.main.hasOwnProperty("temp")) {
+      if (unidades === "metric") {
+        grados = "°C";
+      } else if (unidades === "imperial") {
+        grados = "°F";
+      }
+    }
+
+    const temperatura = document.createElement("div");
+    temperatura.textContent = `${response.main.temp.toFixed(1)} ${grados}`;
+    primerDiv.appendChild(temperatura);
+  }
+  if (
+    response.hasOwnProperty("main") &&
+    response.main.hasOwnProperty("feels_like")
+  ) {
+    const sensTerm = document.createElement("div");
+    sensTerm.textContent = `Sensación de: ${response.main.feels_like.toFixed(
+      1
+    )} ${grados}`;
+    primerDiv.appendChild(sensTerm);
+	}
+	
+  contenedorInfo.appendChild(primerDiv);
+
+  // segundoDiv
+  const segundoDiv = document.createElement("div");
+  segundoDiv.classList.add("segundoDiv");
+
+  if (
+    response.hasOwnProperty("clouds") &&
+    response.clouds.hasOwnProperty("all")
+  ) {
+    const nubosidad = document.createElement("div");
+    nubosidad.textContent = `Nubosidad: ${response.clouds.all}%`;
+    segundoDiv.appendChild(nubosidad);
+  }
+
+	if (
+    response.hasOwnProperty("main") &&
+    response.main.hasOwnProperty("humidity")
+  ) {
+    const humedad = document.createElement("div");
+    humedad.textContent = `Humedad: ${response.main.humidity}%`;
+    segundoDiv.appendChild(humedad);
+  }
+
+	if (
+    response.hasOwnProperty("main") &&
+    response.main.hasOwnProperty("pressure")
+  ) {
+    const presion = document.createElement("div");
+    presion.textContent = `Presión: ${response.main.pressure} hPa`;
+    segundoDiv.appendChild(presion);
+  }
+
+  contenedorInfo.appendChild(segundoDiv);
+
+	// tercerDiv
+	let unidadVelocidad = "";
+	if (unidades === "metric") {
+		unidadVelocidad = "km/h";
+	} else if (unidades === "imperial") {
+		unidadVelocidad = "m/h";
+	}
+
+	const tercerDiv = document.createElement("div");
+	tercerDiv.classList.add("tercerDiv");
+
+	const tituloViento = document.createElement("div");
+	tituloViento.classList.add("tituloViento");
+	tituloViento.textContent = "Viento";
+	tercerDiv.appendChild(tituloViento);
+
+	if (
+    response.hasOwnProperty("wind") &&
+    response.wind.hasOwnProperty("speed")
+  ) {
+		let speedConverted = "";
+		if (unidadVelocidad = "km/h") {
+			speedConverted = response.wind.speed * 3.6;
+		} else if (unidadVelocidad = "m/h") {
+			speedConverted = response.wind.speed;
+		}
+
+    const speed = document.createElement("div");
+    speed.textContent = `Velocidad: ${speedConverted.toFixed(0)} ${unidadVelocidad}`;
+    tercerDiv.appendChild(speed);
+  }
+
+	if (
+    response.hasOwnProperty("wind") &&
+    response.wind.hasOwnProperty("deg")
+  ) {
+		const deg = response.wind.deg;
+		let direccionViento = "";
+		if (deg > 337.5 && deg <= 22.5) {
+			direccionViento = "Norte";
+		} else if (deg > 22.5 && deg <= 67.5) {
+			direccionViento = "Nor-Este";
+		} else if (deg > 67.5 && deg <= 112.5) {
+			direccionViento = "Este";
+		} else if (deg > 112.5 && deg <= 157.5) {
+			direccionViento = "Sur-Este";
+		} else if (deg > 157.5 && deg <= 202.5) {
+			direccionViento = "Sur";
+		} else if (deg > 202.5 && deg <= 247.5) {
+			direccionViento = "Sur-Oeste";
+		} else if (deg > 247.5 && deg <= 292.5) {
+			direccionViento = "Oeste";
+		} else if (deg > 292.5 && deg <= 337.5) {
+			direccionViento = "Nor-Oeste";
+		}
+
+    const direccion = document.createElement("div");
+    direccion.textContent = `Desde el ${direccionViento}`;
+    tercerDiv.appendChild(direccion);
+  }
+
+	if (
+    response.hasOwnProperty("wind") &&
+    response.wind.hasOwnProperty("gust")
+  ) {
+		let speedConverted = "";
+		if (unidadVelocidad = "km/h") {
+			speedConverted = response.wind.gust * 3.6;
+		} else if (unidadVelocidad = "m/h") {
+			speedConverted = response.wind.gust;
+		}
+
+    const gust = document.createElement("div");
+    gust.textContent = `Ráfagas de: ${speedConverted.toFixed(0)} ${unidadVelocidad}`;
+    tercerDiv.appendChild(gust);
+  }
+
+  contenedorInfo.appendChild(tercerDiv);
+
+  console.log(response);
+  infoMeteoPronostico(response.coord.lat, response.coord.lon);
 }
 
-function errorLugarAcoordenadasDOM(error) {
+function mostrarInfoMeteoPronosticoDOM(response) {
+  console.log(response);
+}
+
+function errorLugarAcoordenadasDOM() {
   contenedorInfo.textContent =
-    "Hubo un error buscando el lugar, intente nuevamente.";
+    `Hubo un error buscando el lugar, intente nuevamente. ${error}`;
 }
 
 function errorInfoMeteoDOM(error) {
+  contenedorInfo.textContent = `Hubo un error cargando la información meteorológica, intente nuevamente. ${error}`;
+}
+
+function errorInfoMeteoPronosticoDOM() {
   contenedorInfo.textContent =
-    "Hubo un error cargando la información meteorológica, intente nuevamente.";
+    `Hubo un error cargando el pronóstico meteorológico, intente nuevamente. ${error}`;
 }
 
 export {
   buscarDOM,
   encontradoDOM,
+  mostrarInfoMeteoDOM,
+  mostrarInfoMeteoPronosticoDOM,
   errorLugarAcoordenadasDOM,
   errorInfoMeteoDOM,
-  mostrarInfoMeteoDOM
+  errorInfoMeteoPronosticoDOM
 };
